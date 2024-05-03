7c6acb45314a4a458d58349004151d69b0c87bc5
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-05-03 11:18:19 +0300

    add Habit list views and urls

--- habits/views.py
+++ habits/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 from habits.models import Habit
+from habits.paginators import MyPagination
 from habits.permissions import OwnerPermission
 from habits.serializers import HabitSerializer
 
@@ -27,3 +29,21 @@ class HabitUpdateAPIView(generics.UpdateAPIView):
 class HabitDestroyAPIView(generics.DestroyAPIView):
     queryset = Habit.objects.all()
     permission_classes = [OwnerPermission]
+
+
+class MyHabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    pagination_class = MyPagination
+
+    def get_queryset(self):
+        user = self.request.user
+        return Habit.objects.filter(owner=user).order_by('id')
+
+
+class PublicHabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [AllowAny]
+    pagination_class = MyPagination
+
+    def get_queryset(self):
+        return Habit.objects.filter(is_public=True).order_by('id')
