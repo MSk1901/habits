6708b445e6c1fb96c5fae6d0391c58997b777950
Mariya Kuznetsova <kuznetsova19.m@gmail.com>
2024-06-03 17:17:08 +0300

    update urls

--- habits/urls.py
+++ habits/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
 
 from habits.apps import HabitsConfig
-from habits.views import HabitCreateAPIView, HabitRetrieveAPIView, HabitUpdateAPIView, HabitDestroyAPIView, \
-    MyHabitListAPIView, PublicHabitListAPIView
+from habits.views import HabitCreateAPIView, MyHabitListAPIView, PublicHabitListAPIView, \
+    HabitRetrieveUpdateDestroyAPIView
 
 app_name = HabitsConfig.name
 
 urlpatterns = [
-    path('create/', HabitCreateAPIView.as_view(), name='create'),
-    path('<int:pk>/', HabitRetrieveAPIView.as_view(), name='retrieve'),
-    path('update/<int:pk>/', HabitUpdateAPIView.as_view(), name='update'),
-    path('delete/<int:pk>/', HabitDestroyAPIView.as_view(), name='delete'),
-    path('my-habits/', MyHabitListAPIView.as_view(), name='my-habits'),
+    path('', HabitCreateAPIView.as_view(), name='create'),
+    path('<int:pk>/', HabitRetrieveUpdateDestroyAPIView.as_view(), name='habit'),
+    path('my/', MyHabitListAPIView.as_view(), name='my'),
     path('public/', PublicHabitListAPIView.as_view(), name='public'),
 ]
